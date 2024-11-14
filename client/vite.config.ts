c146a43a286af1ab3176e00043d3e674d8256960
Sinan <muhammedsinan7749@gmail.com>
2024-11-14 22:42:55 +0530

    tailwind added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+import path from "path";
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
